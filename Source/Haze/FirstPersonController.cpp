bb08197ee834ef1add63422643a5b255fedd478c
Thomas <thomasterebesi@googlemail.com>
2019-05-07 21:45:57 +0200

    Added first person controller class.

--- Source/Haze/FirstPersonController.cpp
+++ Source/Haze/FirstPersonController.cpp
@@ -8,6 +8,15 @@ AFirstPersonController::AFirstPersonController()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+
+	SpringArm->SetupAttachment(RootComponent);
+	Camera->SetupAttachment(SpringArm);
+
+	SpringArm->SetWorldLocation(FVector(0.0f, 0.0f, 48.0f));
+	SpringArm->TargetArmLength = 0.0f;
+	SpringArm->bUsePawnControlRotation = true;
 }
 
 // Called when the game starts or when spawned
@@ -29,5 +38,39 @@ void AFirstPersonController::SetupPlayerInputComponent(UInputComponent* PlayerIn
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	// Binding of axis mappings
+	PlayerInputComponent->BindAxis("MoveForward", this, &AFirstPersonController::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AFirstPersonController::MoveRight);
+	PlayerInputComponent->BindAxis("CameraHorizontal", this, &AFirstPersonController::MoveCameraHorizontal);
+	PlayerInputComponent->BindAxis("CameraVertical", this, &AFirstPersonController::MoveCameraVertical);
+
+	// Binding of action mappings
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AFirstPersonController::Jump);
+}
+
+void AFirstPersonController::Jump()
+{
+	Super::Jump();
+}
+
+void AFirstPersonController::MoveForward(float mValue)
+{
+	AddMovementInput(GetActorForwardVector(), mValue);
+	
+}
+
+void AFirstPersonController::MoveRight(float mValue)
+{
+	AddMovementInput(GetActorRightVector(), mValue);
+}
+
+void AFirstPersonController::MoveCameraHorizontal(float mAmount)
+{
+	AddControllerYawInput(mAmount);
+}
+
+void AFirstPersonController::MoveCameraVertical(float mAmount)
+{
+	AddControllerPitchInput(mAmount);
 }
 
--- Source/Haze/FirstPersonController.h
+++ Source/Haze/FirstPersonController.h
@@ -3,7 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+#include "Camera/CameraComponent.h"
+
+#include "Components/InputComponent.h"
+
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+
 #include "FirstPersonController.generated.h"
 
 UCLASS()
@@ -26,4 +33,23 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UCameraComponent* Camera;
+
+	void Jump();
+
+	UFUNCTION(BlueprintCallable)
+	void MoveForward(float mValue);
+
+	UFUNCTION(BlueprintCallable)
+	void MoveRight(float mValue);
+
+	UFUNCTION(BlueprintCallable)
+	void MoveCameraHorizontal(float mAmount);
+
+	UFUNCTION(BlueprintCallable)
+	void MoveCameraVertical(float mAmount);
 };
